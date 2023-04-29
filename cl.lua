21648662f3166356e1f0928f6db31ecf130826c0
Karma243162 <mohammed.tahri24@gmail.com>
2023-04-29 17:54:28 +0200

    Creation des la poubelle + animation ouverture de la poubelle

--- cl.lua
+++ cl.lua
@@ -23,9 +23,18 @@ end, false)
 
 
 for i , pos in ipairs(Config.position) do
-    local object = "prop_ld_rub_binbag_01"
+    local object = "prop_bin_07c"
     CreateObject(object, pos.x, pos.y, pos.z, false, false, true)
+
 end 
+
+function PoubelleOpen() 
+  for i , pos in ipairs(Config.position) do
+      local object2 = "prop_bin_07d"
+      CreateObject(object2, pos.x, pos.y, pos.z, false, false, true)
+        FreezeEntityPosition(object2, true)
+  end 
+end
 
 Citizen.CreateThread(function()
     while true do
@@ -33,10 +42,20 @@ Citizen.CreateThread(function()
 
         local pedCoords = GetEntityCoords(PlayerPedId())
         local objectId =GetClosestObjectOfType(pedCoords, Config.Distance, Config.props, false)
-
-  if DoesEntityExist(objectId) then
+        local objectId2 =GetClosestObjectOfType(pedCoords, Config.Distance, Config.props2, false)
+ 
+        if DoesEntityExist(objectId) then
+        FreezeEntityPosition(objectId, true)
 ESX.ShowHelpNotification('Appuyer sur ~g~E ~w~pour fouiller la poubelle')
 if IsControlJustPressed(1, 51) then
+  Wait(2000)
+  ESX.ShowNotification("Vous venez de trouver ~g~ x3 ~w~ bière !")
+  DeleteObject(objectId)
+  PoubelleOpen() 
+
+ 
+
+
 --[[        RequestAnimDict("missfbi4prepp1")
             while not HasAnimDictLoaded("missfbi4prepp1") do  
                Wait(1)
--- shared/config_spawn.lua
+++ shared/config_spawn.lua
@@ -1,11 +1,17 @@
+
+print("BY KARMA DEV !")
+
+
+
 Config = {}
 Config.Distance = 3.0
-Config.props = GetHashKey("prop_ld_rub_binbag_01")
+Config.props = GetHashKey("prop_bin_07c")
+Config.props2 = GetHashKey("prop_bin_07d")
 
 Config.position = {
       {x = 380.821, y = -1659.499, z = 27.30 - 1},
       {x = 476.83, y = -1671.29, z = 29.28 - 1},
-      {x = 20.0, y = 25.0, z = 35.0}
+      {x = 173.83, y = -1482.19, z = 29.14 - 1}
 }
 
 
